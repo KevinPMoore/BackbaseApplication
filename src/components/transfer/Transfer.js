7f06b94830c5a3c10793abe874bdebadfa5bf06a
Kevin Moore <moore.kevin.p@gmail.com>
2020-10-12 14:36:53 -0400

    added modal to confirm transfer and overdraft logic

--- src/components/transfer/Transfer.js
+++ src/components/transfer/Transfer.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import './Transfer.css';
 
-//add overdraft validation
 //confirm form resets on submission
-//add modal to confirm success
 export default class Transfer extends React.Component{
     state = {
         accounts: [
@@ -31,6 +29,7 @@ export default class Transfer extends React.Component{
         amount: 0,
         checkingBalance: 5824.76,
         error: null,
+        modal: 'none',
         toAccount: ''
     };
 
@@ -39,7 +38,6 @@ export default class Transfer extends React.Component{
     //
     updateAccounts = (ev) => {
         ev.preventDefault();
-        //validation here
         
         let accountIndex = this.state.accounts.findIndex(account => account.name === this.state.toAccount);
         console.log('accountIndex is ', accountIndex);
@@ -58,7 +56,6 @@ export default class Transfer extends React.Component{
 
     updateAmount = (ev) => {
         let transferAmmount = ev.target.value;
-        console.log(transferAmmount);
         if(transferAmmount > (this.state.checkingBalance + 500)) {
             this.setState({
                 amount: 0,
@@ -77,6 +74,32 @@ export default class Transfer extends React.Component{
         });
     };
 
+    updateModal = () => {
+        if(this.state.toAccount === '') {
+            this.setState({
+                error: 'You must select an account to recieve transfered funds.'
+            })
+        } else if(this.state.amount === 0) {
+            this.setState({
+                error: `You must select a transfer amount greater than ${this.state.amount}.`
+            });
+        } else if(this.state.amount > (this.state.checkingBalance + 500)) {
+            this.setState({
+                error: 'This transfer would cause you to exceed your overdraft limit of $500.  Please select a smaller amount.'
+            });
+        } else {
+            if(this.state.modal === 'none') {
+                this.setState({
+                    modal: 'modal'
+                });
+            } else {
+                this.setState({
+                    modal: 'none'
+                });
+            };
+        };
+    };
+
     renderToOptions = () => {
         let transferAccounts = this.state.accounts.map(account =>
             <option key={account.name} value={account.name}>{account.name} {account.balance}</option>
@@ -84,28 +107,41 @@ export default class Transfer extends React.Component{
         return transferAccounts;
     };
 
+    renderConfirmationModal = () => {
+        return(
+            <div className={this.state.modal}>
+                <div className='modal-content'>
+                    <p className='transfer-modal-warning'>You are about to transfer {this.state.amount} from Free Checking(4692) to {this.state.toAccount}.  Are you sure?</p>
+                    <button className='transfer-confirm' onClick={this.updateAccounts}>Confirm</button>
+                    <button className='transfer-cancel' onClick={this.updateModal}>Cancel</button>
+                </div>
+            </div>
+        );
+    };
+
     render(){
         const error = this.state.error;
         return(
             <section className='Transfer'>
+                {this.renderConfirmationModal()}
                 <div className='transfer-title'>
                     <h2>Make a Transfer</h2>
                 </div>
-                <form className='transfer-form' id='transfer-form' onSubmit={this.updateAccounts}>
+                <form className='transfer-form' id='transfer-form'>
                     <fieldset className='transfer-fieldset' form='tf'>
                         <label className='transfer-label' htmlFor='tfa'>From Account</label>
                         <select className='transfer-field' name='transfer-from-account' id='tfa' disabled onChange={this.updateFromAccount}>
                             <option value={this.state.checkingBalance} defaultValue>Free Checking(4692) {this.state.checkingBalance}</option>
                         </select>
                         <label className='transfer-label' htmlFor='tta'>To Account</label>
-                        <select className='transfer-field' name='transfer-to-account' id='tta' onChange={this.updateToAccount}>
+                        <select className='transfer-field' name='transfer-to-account' id='tta' value={this.state.toAccount} onChange={this.updateToAccount}>
                             <option value=''>--Select an Account--</option>
                             {this.renderToOptions()}
                         </select>
                         <label className='transfer-label' htmlFor='amount'>Amount</label>
-                        <input type='number' className='transfer-field' name='transfer-amount' id='ta' min='0' step='0.01' onChange={this.updateAmount}></input>
-                        {error && <p className='error'>This action would cause you to exceed your overdraft limit.  Please select a smaller amount.</p>}
-                        <button className='transfer-button' type='submit'>SUBMIT</button>
+                        <input type='number' className='transfer-field' name='transfer-amount' id='ta' min='0' step='0.01' value={this.state.amount} onChange={this.updateAmount}></input>
+                        {error && <p className='error'>{this.state.error}</p>}
+                        <button className='transfer-button' type='button' onClick={this.updateModal}>SUBMIT</button>
                     </fieldset>
                 </form>
             </section>
